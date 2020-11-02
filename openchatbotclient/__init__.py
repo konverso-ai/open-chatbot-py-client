d3ad7c3cd57925cabf61b59437ed17251613902f
Amedee Potier <amedee.potier@konverso.ai>
2020-11-02 20:55:20 +0000

    KB-8534: adding response, response_group and client_group

--- openchatbotclient/__init__.py
+++ openchatbotclient/__init__.py
@@ -1,7 +1,10 @@
 """Client for Open Chat Bot's"""
 
 from .client import client
+from .client_group import client_group
 from .descriptor import descriptor
 from .repository import repository
+from .response import response
+from .response_group import response_group
 
 name = "openchatbotlient"
