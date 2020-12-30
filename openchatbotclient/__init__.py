e45fce100190b286f3e14a4462be446394a59c64
Amedee Potier <amedee.potier@konverso.ai>
2020-12-30 15:01:07 +0000

    KB-8933: Add package inner documentation

--- openchatbotclient/__init__.py
+++ openchatbotclient/__init__.py
@@ -1,4 +1,45 @@
-"""Client for Open Chat Bot's"""
+"""
+Open Chat Bot Client package
+============================
+
+This Python package contains utilities that allow you to find, access, and use bots that are compliant with the Alliance for Open Chatbot standard. The package is designed for the Python 3 environments.
+
+The Open Chatbot standard
+-------------------------
+
+The implementation is based on the standard defined by the Alliance.
+ - The Alliance: https://www.alliance-open-chatbot.org/
+ - The standard: https://github.com/alliance-for-openchatbot/standard
+
+Authors
+-------
+
+The initial implementation is made by Konverso in 2020 by Alexander Danilov and Amedee Potier (amedee.potier@konverso.ai).
+
+See also
+--------
+
+ - The API standard specifications: https://github.com/alliance-for-openchatbot/standard
+ - The definition of the standard bot descriptor: https://openchatbot.io/domainbots
+ - The easy-to-use web client that adds a widget connected to any chatbot to your website: https://github.com/ohoachuck/openchatbot-webclient
+
+License
+-------
+
+This package is released under the MIT license. To learn more about it, view the LICENSE file in this folder.
+
+Usage
+-----
+
+To get started, either view the test.py file in this module, or consult the help on the client module:
+
+    from openchatbotclient import client
+    help(client)
+
+You may also find complete examples and tutorials in the related GitHub repository:
+    https://github.com/konverso-ai/open-chatbot-py-client
+
+"""
 
 from .client import Client
 from .descriptor import Descriptor
