1213d5b56bf9d5cfa8385e483d257db803c719f2
Amedee Potier <amedee.potier@konverso.ai>
2020-12-30 14:53:23 +0000

    KB-8933: Improve package info

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="open-chat-bot-client",
-    version="0.0.1",
+    name="open-chatbot-py-client",
+    version="0.1.0",
     author="Konverso",
-    author_email="",
-    description="Client for Open Chat Bot",
+    author_email="contact@konverso.ai",
+    description="Client to access any chatbot compliant with the Alliance for Open Chatbot standard",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://konverso.ai",
+    url="https://www.konverso.ai",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
